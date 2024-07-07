268ebf919b7bf9ed97a411411925f35a8e154a02
agusotoholt <168455366+agusotoholt@users.noreply.github.com>
2024-07-07 21:54:10 +0000

    subida contador

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,37 @@
 import React from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faClock } from '@fortawesome/free-regular-svg-icons'; 
+
 
 //create your first component
-const Home = () => {
+const Home = ({digito1,digito2,digito3,digito4,digito5,digito6}) => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container-fluid d-flex justify-content-between bg-dark" style={{ height: '75px' }}>
+			<div className="rounded m-auto d-flex justify-content-center align-items-center" style={{ height: '65px', width: '50px', backgroundColor: 'rgba(255, 255, 255, 0.03)', fontSize: '30px' }}>
+				<FontAwesomeIcon icon={faClock} style={{ color: '#ffffff' }} />
+			</div>
+			<div className="rounded m-auto d-flex justify-content-center align-items-center" style={{ height: '65px', width: '50px', backgroundColor: 'rgba(255, 255, 255, 0.03)', fontSize: '30px' }}>
+				<p className="fw-bold text-white w-100 h-100 d-flex justify-content-center align-items-center m-0">{digito1}</p>
+			</div>
+			<div className="rounded m-auto d-flex justify-content-center align-items-center" style={{ height: '65px', width: '50px', backgroundColor: 'rgba(255, 255, 255, 0.03)', fontSize: '30px' }}>
+				<p className="fw-bold text-white w-100 h-100 d-flex justify-content-center align-items-center m-0">{digito2}</p>
+			</div>
+			<div className="rounded m-auto d-flex justify-content-center align-items-center" style={{ height: '65px', width: '50px', backgroundColor: 'rgba(255, 255, 255, 0.03)', fontSize: '30px' }}>
+				<p className="fw-bold text-white w-100 h-100 d-flex justify-content-center align-items-center m-0">{digito3}</p>
+			</div>
+			<div className="rounded m-auto d-flex justify-content-center align-items-center" style={{ height: '65px', width: '50px', backgroundColor: 'rgba(255, 255, 255, 0.03)', fontSize: '30px' }}>
+				<p className="fw-bold text-white w-100 h-100 d-flex justify-content-center align-items-center m-0">{digito4}</p>
+			</div>
+			<div className="rounded m-auto d-flex justify-content-center align-items-center" style={{ height: '65px', width: '50px', backgroundColor: 'rgba(255, 255, 255, 0.03)', fontSize: '30px' }}>
+				<p className="fw-bold text-white w-100 h-100 d-flex justify-content-center align-items-center m-0">{digito5}</p>
+			</div>
+			<div className="rounded m-auto d-flex justify-content-center align-items-center" style={{ height: '65px', width: '50px', backgroundColor: 'rgba(255, 255, 255, 0.03)', fontSize: '30px' }}>
+				<p className="fw-bold text-white w-100 h-100 d-flex justify-content-center align-items-center m-0">{digito6}</p>
+			</div>
 		</div>
+		
 	);
 };
 
--- src/js/index.js
+++ src/js/index.js
@@ -8,6 +8,44 @@ import "../styles/index.css";
 //import your own components
 import Home from "./component/home.jsx";
 
+let num6 = 0, num5 = 0, num4 = 0, num3 = 0, num2 = 0, num1 = 0;
+
+setInterval(function() {
+    num6 >= 9 ? (num6 = 0, num5 >= 9 ? (num5 = 0, num4 >= 9 ? (num4 = 0, num3 >= 9 ? (num3 = 0, num2 >= 9 ? (num2 = 0, num1 >= 9 ? num1 = 0 : num1++) : num2++) : num3++) : num4++) : num5++) : num6++;
+    // if (num6 >=9) {
+    //     num6 = 0;
+    // if (num5 >=9) {
+    //     num5 = 0;
+    // if (num4 >=9) {
+    //     num4 = 0;
+    // if (num3 >=9) {
+    //     num3 = 0;
+    // if (num2 >=9) {
+    //     num2 = 0;
+    // if (num1 >=9) {
+    //     num1 = 0;
+    // } else {
+    //     num1++;
+    // }
+    // } else {
+    //     num2++;
+    // }
+    // } else {
+    //     num3++;
+    //     }
+    // } else {
+    //     num4++;
+    // }
+    // } else {
+    //     num5++;
+    // }
+    // } else {
+    //     num6++;
+    // };
+
 //render your react application
-ReactDOM.createRoot(document.getElementById('app')).render(<Home/>);
+ReactDOM.createRoot(document.getElementById('app')).render(<Home digito6={num6} digito5={num5} digito4={num4} digito3={num3} digito2={num2} digito1={num1}/>);
+}, 1000)
+
+
 
